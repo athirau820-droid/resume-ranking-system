4c7e9c3135c6ad5c91c95a573f0884c756b970b7
athirau820-droid <athirau820@gmail.com>
2026-03-08 10:38:29 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -4,20 +4,22 @@ const path = require("path");
 
 const app = express();
 
+// Middleware
+app.use(cors()); // MUST be before routes
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
+
 // Import routes
 const candidateRoute = require("./routes/candidates");
 const analyzeRoute = require("./routes/analyze");
 const jobRoute = require("./routes/jobs");
-
-// Middleware
-app.use(cors());
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true }));
+const loginRoutes = require("./routes/login");
 
 // Resume upload folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Routes
+app.use("/api", loginRoutes);
 app.use("/api/candidates", candidateRoute);
 app.use("/api/analyze", analyzeRoute);
 app.use("/api/jobs", jobRoute);
